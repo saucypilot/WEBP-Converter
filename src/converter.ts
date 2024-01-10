71f7f29d6b9046d0a526e39aea309685ce324234
Irian Durian <durianirian@gmail.com>
2024-01-09 21:23:06 -0600

    Update

--- src/converter.ts
+++ src/converter.ts
@@ -1,10 +1,11 @@
-import { App } from 'obsidian';
-import webp from 'webp-converter';
-import { TFile, TAbstractFile } from 'obsidian';
+import webp from "webp-converter";
+import { TFile } from "obsidian";
 
-const webp=require('webp-converter');
 webp.grant_permission(); // this will grant 755 permission to webp executables
 
-export async function convertImageToWebp() {
-
+export async function convertImageToWebp(inputFile: TFile, destination: string) {
+    const filePath = inputFile.path;
+    await webp.cwebp(filePath, destination, "-q 80", function(status: unknown, error: unknown){
+        console.log(status, error);
+    });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+import { Menu, Notice, Plugin, Vault} from "obsidian";
+import { MouseEvent } from 'react';
 import { convertImageToWebp } from "./converter";
-import { TFile } from "obsidian";
+import { TFile } from "obsidian"; 
 
+export default class WEBPConverterPlugin extends Plugin {
+	async onload() {
+		console.log("loading plugin");
+		const webpMenu = new Menu();
+
+		webpMenu.addItem((item)	=>
+			item.setTitle("Convert to WebP").onClick(() => {
+				convertImageToWebp( this.app.workspace.getActiveFile(), /* destination should be where the original image was */ );
+				new Notice("Converted to WebP");
+			})
+		);
+
+		webpMenu.showAtMouseEvent(MouseEvent);
+	}
+
+	onunload() {
+		console.log("unloading plugin");
+	}
+}
